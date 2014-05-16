11ce8ff5d0768db1e665ccb5ecc73b10c86d35f8
Austen Ito <austen.ito@gmail.com>
2014-05-15 20:05:02 -0400

    Simplyfy gem to match happiness service.

--- lib/poptart/question.rb
+++ lib/poptart/question.rb
@@ -2,9 +2,24 @@ module Poptart
   class Question
     extend Poptart::Request
 
+    attr_accessor :id
+
     def initialize(params)
       @id = params['id']
-      @survey_id = params['survey_id']
+    end
+
+    def self.all(params = {})
+      root = Poptart::Root.get_root
+      response = get(root.links.questions.href)
+      JSON.parse(response.body).map do |question|
+        if params[:type]
+          if question['question_type'] == params[:type]
+            new(question)
+          end
+        else
+          new(question)
+        end
+      end.compact
     end
   end
 end
--- spec/requests/survey_spec.rb
+++ spec/requests/survey_spec.rb
@@ -22,28 +22,22 @@ describe Poptart::Survey do
     survey.survey_questions.first.answer.should == "foo"
   end
 
-  it "answers a boolean question", :vcr, record: :all do
-    boolean_questions = Poptart::BooleanQuestion.all
+  it "answers a survey question", :vcr do
+    boolean_questions = Poptart::Question.all(type: 'boolean')
     survey = Poptart::Survey.create
     survey.add_question(boolean_questions.first).should be
 
     survey = Poptart::Survey.for_id(survey.id)
     survey.survey_questions.count.should == 1
     survey_question = survey.survey_questions.first
-    survey_question.type.should == "BooleanQuestion"
+    survey_question.type.should == 'boolean'
 
-    survey_question.answer = true
+    survey_question.answer = 'true'
     survey_question.submit.should be
 
     survey = Poptart::Survey.for_id(survey.id)
-    survey.survey_questions.first.answer.should == true
+    survey.survey_questions.first.answer.should == 'true'
   end
-
-  it "returns a multiple response question"
-
-  it "returns a range question"
-
-  it "returns a time range question"
 end
 
 
